7da9dfd1f2b17ec76a898dac3e0773dea78d6a0e
Patrik Fatura <patofatura@gmail.com>
2020-10-19 13:04:25 +0200

    Done

--- Open-Lab-01.04/Triangle.cs
+++ Open-Lab-01.04/Triangle.cs
@@ -6,7 +6,10 @@ namespace Open_Lab_01._04
     {
         public double Area(double foundation, double height)
         {
-            throw new NotImplementedException();
+           int a = Convert.ToInt32(foundation);
+            int b = Convert.ToInt32(height);
+            double Area = a + b;
+            return Area;
         }
     }
 }
